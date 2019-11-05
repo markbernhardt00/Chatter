497968f769584fb02729b9786d8fcdfefb1a8ece
mruck16 <michaelruck19@gmail.com>
2019-11-05 02:12:56 -0600

    -Added Views

--- Chatter/ViewController.swift
+++ Chatter/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var signInOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        roundButton()
+    
+    }
+    
+    func roundButton() {
+        signInOutlet.layer.cornerRadius = 5
+        signInOutlet.layer.borderWidth = 1
     }
 
-
+    @IBAction func signInAction(_ sender: Any) {
+        showMessageBoardController(self)
+    }
+    
+    @IBAction func showMessageBoardController(_ sender: Any) {
+      performSegue(withIdentifier: "messageBoardSegue", sender: self)
+        
+    }
+    
 }
 
