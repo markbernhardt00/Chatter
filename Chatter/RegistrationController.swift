942e89b3f13ea0358c6b485f48b072a6acb942a5
Mark Bernhardt <markbernhardt00@gmail.com>
2019-11-05 18:18:08 -0600

    added outlets and hashing to create account

--- Chatter/RegistrationController.swift
+++ Chatter/RegistrationController.swift
@@ -11,15 +11,42 @@ import UIKit
 class RegistrationController: UIViewController {
 
     @IBOutlet weak var createAccountOutlet: UIButton!
+    @IBOutlet weak var fNameOutlet: UITextField!
+    @IBOutlet weak var lNameOutlet: UITextField!
+    @IBOutlet weak var emailOutlet: UITextField!
+    @IBOutlet weak var usernameOutlet: UITextField!
+    @IBOutlet weak var passwordOutlet: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         roundButton()
     }
     
+    func passwordHash(from username: String, password: String) -> String {
+      let salt = "x4vV8bGgqqmQwgCoyXFQj+(o.nUNQhVP7ND"
+      return "\(password).\(username).\(salt)".sha256()
+    }
+    
     @IBAction func createAccountBtn(_ sender: Any) {
+        guard let fName = fNameOutlet.text, fName.count > 0 else {
+            return
+        }
+        guard let lName = lNameOutlet.text, lName.count > 0 else {
+            return
+        }
+        guard let email = emailOutlet.text, email.count > 0 else {
+            return
+        }
+        guard let username = usernameOutlet.text, username.count > 0 else {
+            return
+        }
+        guard let password = passwordOutlet.text, password.count > 0 else {
+            return
+        }
+        
+        let hash = passwordHash(from: username, password: password)
+        print(hash)
         showMessageBoardViewController(self)
     }
     
@@ -30,6 +57,11 @@ class RegistrationController: UIViewController {
     func roundButton() {
         createAccountOutlet.layer.cornerRadius = 5
         createAccountOutlet.layer.borderWidth = 1
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
     }
     
     /*
